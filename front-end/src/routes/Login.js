d9052b8c62d47ad563518fe1b6b55528ffd7ed88
computerdre24 <43916157+computerdre24@users.noreply.github.com>
2020-11-09 16:25:17 -0800

    Incomplete skeleton of Login page

--- front-end/src/routes/Login.js
+++ front-end/src/routes/Login.js
@@ -1,11 +1,38 @@
 import React, {useContext} from 'react';
 import { Redirect } from 'react-router-dom';
+import { Form, FormGroup, FormControl, ControlLabel, FlexboxGrid } from 'rsuite';
+import { Grid, Row, Col } from 'rsuite';
+import { Button } from 'rsuite';
+
 
 const Login = ({ history }) => {
         return (
-            <div>
-                <h1>Login page</h1>
-            </div>
+            <FlexboxGrid colspan={20} justify="center">
+                <FlexboxGrid.Item>
+                    <Col>
+                        <h1 align="center">Login</h1>
+                        <img src="../images/where2study.png" height={300} width={300}></img>
+                        <Form>
+                            <FormGroup>
+                                <ControlLabel>Email</ControlLabel>
+                                <FormControl name="name" type="email" />
+
+                                <ControlLabel>Password</ControlLabel>
+                                <FormControl name="email" type="password" />
+                            </FormGroup>
+                        </Form>
+
+                        <FlexboxGrid.Item>
+                            <Row>
+                                <div className="buttons"></div>
+                                <Button appearance="primary">Sign In</Button>
+                                {" "}
+                                <Button appearance="primary" color="green">Sign Up</Button>
+                            </Row>
+                        </FlexboxGrid.Item>
+                    </Col>
+                </FlexboxGrid.Item>
+            </FlexboxGrid>
         )
 };
 
