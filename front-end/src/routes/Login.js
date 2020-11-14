3002cea78d079a333dea6e9a0c1b945160aa94f5
Carlos <ccarvaja@ucsd.edu>
2020-11-13 19:19:17 -0800

    Fixed FlexboxGrid Form, created a login class, works with login

--- front-end/src/routes/Login.js
+++ front-end/src/routes/Login.js
@@ -1,40 +1,76 @@
 import React, {useContext} from 'react';
 import { Redirect } from 'react-router-dom';
-import { Form, FormGroup, FormControl, ControlLabel, FlexboxGrid } from 'rsuite';
+import { Form, FormGroup, FormControl, ControlLabel, FlexboxGrid, ButtonToolbar, HelpBlock } from 'rsuite';
 import { Grid, Row, Col } from 'rsuite';
 import { Button } from 'rsuite';
+import db from "../base"
 import logo from '../images/where2study.png';
 
 
-const Login = ({ history }) => {
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            formValue: {
+                email: '',
+                password: ''
+            }
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+    }
+
+    handleLogin() {
+        const { email, password } = this.state.formValue;
+
+        try{
+            db
+                .auth()
+                .signInWithEmailAndPassword(email, password);
+                this.props.history.push("/");
+        } catch(error){
+            alert(error);
+        }
+    }
+
+    handleChange(value) {
+        this.setState({
+            formValue: value
+        });
+    }
+
+    render() {
         return (
-            <FlexboxGrid colspan={20} justify="center">
-                <FlexboxGrid.Item>
-                    <Col>
-                        <h1 align="center">Login</h1>
-                        <img src={logo} height={300} width={300}></img>
-                        <Form>
-                            <FormGroup>
-                                <ControlLabel>Email</ControlLabel>
-                                <FormControl name="name" type="email" />
-
-                                <ControlLabel>Password</ControlLabel>
-                                <FormControl name="email" type="password" />
-                            </FormGroup>
-                        </Form>
-
-                        <FlexboxGrid.Item>
-                            <Row>
-                                <div className="buttons"></div>
-                                <Button appearance="primary">Sign In</Button>
-                                {" "}
-                                <Button appearance="primary" color="green">Sign Up</Button>
-                            </Row>
-                        </FlexboxGrid.Item>
-                    </Col>
-                </FlexboxGrid.Item>
-            </FlexboxGrid>
-        )
-};
+            <div className="show-login">
+                <FlexboxGrid colspan={20} justify="center">
+                    <FlexboxGrid.Item>
+                        <Col>
+                            <h1 align="center">Login</h1>
+                            <img src={logo} height={300} width={300} />
+                            <Form onChange={this.handleChange} formValue={this.state.formValue}>
+                                <FormGroup>
+                                    <ControlLabel>Email</ControlLabel>
+                                    <FormControl name="email" type="email" placeholder="Email"/>
+                                    <HelpBlock tooltip>Required</HelpBlock>
+                                </FormGroup>
+                                <FormGroup>
+                                    <ControlLabel>Password</ControlLabel>
+                                    <FormControl name="password" type="password" placeholder="Password" />
+                                    <HelpBlock tooltip>Required</HelpBlock>
+                                </FormGroup>
+                                <FormGroup>
+                                    <ButtonToolbar>
+                                        <Button onClick={this.handleLogin} appearance="primary">Sign In</Button>
+                                        <Button appearance="primary" color="green">Sign Up</Button>
+                                    </ButtonToolbar>
+                                </FormGroup>
+                            </Form>
+                        </Col>
+                    </FlexboxGrid.Item>
+                </FlexboxGrid>
+            </div>
+        );
+    }
+}
 
 export default Login;
