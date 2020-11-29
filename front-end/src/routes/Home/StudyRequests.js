db1b98d259c69c9407e01cd495caecf63fd73f37
adib thaqif <mtaufek@ucsd.edu>
2020-11-30 04:02:53 +0800

    Home page layout v3

--- front-end/src/routes/Home/StudyRequests.js
+++ front-end/src/routes/Home/StudyRequests.js
@@ -3,6 +3,7 @@ import {Col, Rate, Icon, FlexboxGrid, Panel, Message} from "rsuite";
 import cseBuilding from "../../images/cse-building.jpeg"
 import priceCenter from "../../images/price-center.jpg"
 import geiselLibrary from "../../images/geisel-libary.jpg"
+import atkinsonHall from "../../images/atkinson-hall.jpg"
 
 class StudyRequests extends Component{
     render(){
@@ -66,7 +67,28 @@ class StudyRequests extends Component{
                     <Rate readOnly defaultValue={3} size="sm" character={<Icon icon="volume-up" style={{ color: 'rgba(0, 106, 150, 0.75)' }} />} /> 
                 </Col>
             </Panel>
-          </FlexboxGrid.Item>               
+          </FlexboxGrid.Item>   
+          <FlexboxGrid.Item colspan={6}>
+            <Panel shaded bordered bodyFill style={{ display: 'inline-block', width: 240 }}>
+                <img src={geiselLibrary} height="240" />
+                <Message
+                    
+                    style={{backgroundColor: 'white'}}
+                    title="Go over Review Quiz 2"
+                    description={
+                        <p>
+                            CSE 12
+                            <br />
+                            Atkinson Hall
+                        </p>
+                    }
+                />
+                <Col xs={24}> 
+                    <Rate readOnly defaultValue={2} size="sm" character={<Icon icon="volume-up" style={{ color: 'rgba(0, 106, 150, 0.75)' }} />} /> 
+                </Col>
+            </Panel>
+          </FlexboxGrid.Item> 
+                      
         </FlexboxGrid>
         )
     }
