be6232fffc29a709045804e682e0ae4e0eeedb31
Brian Sanders <brian@Brians-MacBook-Pro.local>
2020-12-01 20:32:04 -0800

    removed unused code

--- front-end/src/routes/Home.js
+++ front-end/src/routes/Home.js
@@ -1,21 +1,6 @@
 import React, { useContext } from 'react';
 import { DataContext } from '../state/context';
 
-
-// Note will need to implement Unit 4 in order to adjust to changes in DB
-
-// class Home extends Component {
-//     constructor(props) {
-//         super(props);
-//     }
-
-//     render() {
-//         return (
-//             <h1>Home page</h1>
-//         )
-//     }
-// };
-
 function Home() {
     const { state, dispatch } = useContext(DataContext);
     console.log(state);
