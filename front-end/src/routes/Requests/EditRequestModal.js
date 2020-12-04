4abcc44a2f043ce7b0e56a1a73cacf02c6dac642
Brian <brian.solis717@gmail.com>
2020-12-03 21:50:48 -0800

    Work on EditRequestModal

--- front-end/src/routes/Requests/EditRequestModal.js
+++ front-end/src/routes/Requests/EditRequestModal.js
@@ -1,3 +1,4 @@
+import classes from '*.module.css';
 import React, {Component} from 'react';
 import ButtonToolbar, { Button, Modal, Grid, Row, Col, Rate, Container, Header,
     Content, Footer, Sidebar, Divider, List, Tooltip, Whisper, Alert, Progress, Form, FormGroup, FormControl,
@@ -5,9 +6,11 @@ import ButtonToolbar, { Button, Modal, Grid, Row, Col, Rate, Container, Header,
 import FlexboxGrid from "rsuite";
 import styled from 'styled-components'
 import ConfirmDeleteModal from './ConfirmDeleteModal'
+import db from '../../base';
 const max_chars = 100;
 const alert_time = 1250
-var locations = []
+var locations = [];
+var classes = [];
 
 const ButtonContainer = styled.div`display: flex; justify-content: space-between;`;
 
@@ -31,11 +34,140 @@ class Home extends React.Component {
         super(props);
         this.state = {
             show: false,
-            showConfirmModal: false
+            showConfirmModal: false,
+            formValue: {
+                title: '',
+                class: '',
+                location: '',
+                noise_level: '',
+                end_time: new Date(),
+                description: '',
+              },
+              formError: {},
+              char_left = max_chars
+
+
         };
+        this.handleConfirmEdits = this.handleConfirmEdits.bind(this);
+        this.handleDeletion = this.handleDeletion.bind(this);
+        this.editRequest = this.editRequest.bind(this);
         this.close = this.close.bind(this);
         this.open = this.open.bind(this);
+        
+
+       
+        this.user = db.auth().currentUser; // Reference to Current User From Auth Database
+        this.uid = this.user.uid;  // Current User's Identifier
     }
+
+
+     getCurrentLocation = () =>{
+        var ref = firebase.database().ref("Locations");
+        ref.once("value")
+        .then(function(snapshot) {
+          return snapshot
+      });
+
+    }
+
+    getCurrentClasses = () =>{
+        var ref = firebase.database().ref("Classes");
+        ref.once("value")
+        .then(function(snapshot) {
+          return snapshot
+      });
+
+    }
+
+    getPreviousInformation = () =>{
+        var ref = firebase.database().ref("RequestsList/" + uid);
+       ref.once("value")
+         .then(function(snapshot) {
+           return snapshot
+       });
+    }
+
+    componentDidMount(){
+        this.getCurrentLocation().then(locationsSnapshot=>{
+            locationsSnapshot.forEach(function(childSnapshot){
+                locations.push({
+                    "label" : childSnapshot.key.toString(),
+                    "value": childSnapshot.key.toString(),
+                    "role": "Master"
+
+                });
+            });
+        });
+        
+        this.getCurrentClasses().then(classesSnapshot =>{
+            classesSnapshot.forEach(function(childSnapshot){
+                classes.push(
+                    {
+                        "label": childSnapshot.key.toString(),
+                        "value": childSnapshot.key.toString(),
+                        "role": "Master"
+                    }
+                );
+            });
+        });
+
+        this.getPreviousInformation().then(requestSnapshot =>{
+            this.setState({
+                formValue:{
+                 class: requestSnapshot.child("Class").val(),
+                 description: requestSnapshot.child("description").val(),
+                 location: requestSnapshot.child("location").val(),
+                 noise_level: requestSnapshot.child("noiseRating").val(),
+                 end_time: requestSnapshot.child("study_end").val()
+                  }})
+            this.uid = requestSnapshot.key
+            
+           
+        })
+
+        
+    }
+    
+    handleDeletion(){
+        this.setState({showConfirmModal: true})
+        var adaRef = firebase.database().ref('RequestsList'+ this.uid);
+           adaRef.remove()
+         .then(function() {
+             console.log("Remove succeeded.")
+          })
+         .catch(function(error) {
+            console.log("Remove failed: " + error.message)
+        });
+        this.close()
+    }
+    
+    
+    
+    handleConfirmEdits(){
+        if( !this.form.check() ){
+            Alert.error('Please fix the highlighted fields', alert_time);
+
+        } else {
+            // No error occurred handle accordingly
+            this.editRequest();
+        }
+    }
+
+   editRequest = () =>{
+    var ref = firebase.database().ref('RequestsList'+ this.uid);
+     ref.child('USERID').set(this.uid);
+     ref.child('class').set(this.formValue.class);
+     ref.child('description').set(this.formValue.description);
+     ref.child('location').set(this.formValue.location);
+     ref.child('noiseRating').set(this.formValue.noise_level);
+     ref.child('study_end').set(this.formValue.study_end);
+       
+     
+     this.close()
+   }
+
+
+
 
     /**
      * Set the state of the modal to stop showing and calls parent component
@@ -195,7 +327,7 @@ class Home extends React.Component {
                                              speaker={<Tooltip>Do you want to delete to this study request?</Tooltip>} >
                                         <Button 
                                             color="red" 
-                                            onClick={ ()=>{this.setState({showConfirmModal: true})} } 
+                                            onClick={this.handleDeletion} 
                                             appearance="primary">
                                             DELETE
                                         </Button>
@@ -205,7 +337,7 @@ class Home extends React.Component {
                                                  trigger="hover"
                                                  speaker={<Tooltip>Do you want to confirm edits to this study request?</Tooltip>}>
                                             <Button 
-                                                onClick={this.close} 
+                                                onClick={this.handleConfirmEdits} 
                                                 appearance="primary">
                                                 CONFIRM EDITS
                                             </Button>
