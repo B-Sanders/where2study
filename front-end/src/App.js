09e1ec18bd03dbfe054154fa8c8e218f2e6fdff9
Nick <nweaver@ucsd.edu>
2020-12-03 16:07:48 -0800

    Modal now appears when you click on study request card

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -13,22 +13,38 @@ import ProfileEdit from "./routes/Profile/ProfileEdit.js";
 import Profile from "./routes/Profile/Profile.js";
 import HomePage from "./routes/Home/index.js";
 
+import { createGlobalStyle } from "styled-components"
+import { isStyledComponent } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body{
+    height:100%
+  }
+
+  #app div{
+    height:100%
+  }
+`;
+
 function App() {
   const [state, dispatch] = useReducer(dataReducer, initialDataState);
   return (
-    <AuthProvider>
-      <DataContext.Provider value={{ state, dispatch }}>
-        <Router>
-          <div>
-            <PrivateRoute exact path="/" component={HomePage} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/profileEdit" component={ProfileEdit} />
-            <Route exact path="/profile" component={Profile} />
-          </div>
-        </Router>
-      </DataContext.Provider>
-    </AuthProvider>
+    <div>
+      <GlobalStyles />
+        <AuthProvider>
+          <DataContext.Provider value={{ state, dispatch }}>
+            <Router>
+              <div>
+                <PrivateRoute exact path="/" component={HomePage} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/signup" component={Signup} />
+                <Route exact path="/profileEdit" component={ProfileEdit} />
+                <Route exact path="/profile" component={Profile} />
+              </div>
+            </Router>
+          </DataContext.Provider>
+        </AuthProvider>
+    </div>
   );
 }
 
