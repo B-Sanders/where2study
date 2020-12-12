7e4d9432f6d7d80ab19adda84baf1082eefec262
Carlos <ccarvaja@ucsd.edu>
2020-12-12 14:05:16 -0800

    added cancel button to profile edit page

--- front-end/src/routes/Profile/ProfileEdit.js
+++ front-end/src/routes/Profile/ProfileEdit.js
@@ -7,7 +7,12 @@ import {
   ControlLabel,
   FlexboxGrid,
   ButtonToolbar,
-  Alert, RadioGroup, Radio, InputPicker, Header, Container,
+  Alert,
+  RadioGroup,
+  Radio,
+  InputPicker,
+  Header,
+  Container,
 } from "rsuite";
 import { Grid, Row, Col } from "rsuite";
 import { Button } from "rsuite";
@@ -15,12 +20,12 @@ import { TagPicker } from "rsuite";
 import db from "../../base";
 import majorData from "../majors.json";
 import classData from "../courses.json";
-import SideBar from "../../Header"
-import styled from 'styled-components';
+import SideBar from "../../Header";
+import styled from "styled-components";
 import { DataContext } from "../../state/context";
 import { UPDATE_USER } from "../../state/actions";
 import logo from "../../images/where2study.png";
-import { getUser, editProfile } from '../../utils/fetches';
+import { getUser, editProfile } from "../../utils/fetches";
 
 const ProfileEditContainer = styled.div`
   width: 100%;
@@ -39,23 +44,18 @@ class ProfileEdit extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      display_name: '',
+      display_name: "",
       major: [],
       classes: [],
-      pronouns: '',
+      pronouns: "",
     };
     this.updateProfile.bind(this);
     this.backToProfile.bind(this);
   }
 
   updateProfile = () => {
-    let {
-      display_name,
-      major,
-      classes,
-      pronouns,
-    } = this.state;
-    if (pronouns === '') {
+    let { display_name, major, classes, pronouns } = this.state;
+    if (pronouns === "") {
       pronouns = this.context.state.user.pronouns;
     }
 
@@ -72,108 +72,123 @@ class ProfileEdit extends React.Component {
 
   backToProfile = () => {
     this.props.history.push("/profile");
-  }
+  };
 
   componentDidMount() {
-    const userId = window.localStorage.getItem('loginToken');
+    const userId = window.localStorage.getItem("loginToken");
     getUser(userId).then((res) => {
-      console.log('res.major', res.major);
+      console.log("res.major", res.major);
       this.context.dispatch({
-          type: UPDATE_USER,
-          payload: {
-              user: {
-              active_post: res.active_post,
-              classes: res.classes,
-              display_name: res.display_name,
-              email: res.email,
-              major: res.major,
-              pronouns: res.pronouns,
-              uuid: res.uuid,
-              },
+        type: UPDATE_USER,
+        payload: {
+          user: {
+            active_post: res.active_post,
+            classes: res.classes,
+            display_name: res.display_name,
+            email: res.email,
+            major: res.major,
+            pronouns: res.pronouns,
+            uuid: res.uuid,
           },
+        },
       });
-        this.setState({
-          display_name: this.context.state.user.display_name,
-          major: this.context.state.user.major.split(','),
-          classes: this.context.state.user.classes,
-          pronouns: this.context.state.user.pronouns,
+      this.setState({
+        display_name: this.context.state.user.display_name,
+        major: this.context.state.user.major.split(","),
+        classes: this.context.state.user.classes,
+        pronouns: this.context.state.user.pronouns,
       });
-  });
-}
+    });
+  }
 
   render() {
     return (
       <ProfileEditContainer>
-        <SideBar history={this.props.history}/>
+        <SideBar history={this.props.history} />
         <ProfileContainer>
-        <FlexboxGrid colspan={20} justify="center">
-          <FlexboxGrid.Item>
-            <Col>
-              <Header>
-                <FlexboxGrid justify="center">
-                  <FlexboxGrid.Item>
-                    <img src={logo} height="125" width="150" href="/"/>
-                  </FlexboxGrid.Item>
-                </FlexboxGrid>
-              </Header>
-              <h1 align="center">Profile Edit</h1>
-              <Form>
-                <FormGroup>
-                  <ControlLabel>User ID</ControlLabel>
-                  <FormControl
-                    name="display_name"
-                    type="text"
-                    maxlength="15"
-                    onChange={(newValue) => { this.setState({ display_name: newValue }); }}
-                    value={this.state.display_name}
-                  />
+          <FlexboxGrid colspan={20} justify="center">
+            <FlexboxGrid.Item>
+              <Col>
+                <Header>
+                  <FlexboxGrid justify="center">
+                    <FlexboxGrid.Item>
+                      <img src={logo} height="125" width="150" href="/" />
+                    </FlexboxGrid.Item>
+                  </FlexboxGrid>
+                </Header>
+                <h1 align="center">Profile Edit</h1>
+                <Form>
+                  <FormGroup>
+                    <ControlLabel>User ID</ControlLabel>
+                    <FormControl
+                      name="display_name"
+                      type="text"
+                      maxlength="15"
+                      onChange={(newValue) => {
+                        this.setState({ display_name: newValue });
+                      }}
+                      value={this.state.display_name}
+                    />
 
-                  <ControlLabel>Major</ControlLabel>
+                    <ControlLabel>Major</ControlLabel>
                     <TagPicker
                       data={majorData}
                       groupBy="department"
                       defaultValue={this.state.major}
                       value={this.state.major}
                       style={{ width: 300 }}
-                      onChange={(val) => { this.setState({ major: val })}}
+                      onChange={(val) => {
+                        this.setState({ major: val });
+                      }}
                     />
 
-                  <ControlLabel>Classes</ControlLabel>
+                    <ControlLabel>Classes</ControlLabel>
                     <TagPicker
                       data={classData}
                       groupBy="department"
                       defaultValue={this.state.classes}
                       value={this.state.classes}
-                      onChange={(val) => { this.setState({ classes: val })}}
+                      onChange={(val) => {
+                        this.setState({ classes: val });
+                      }}
                       style={{ width: 300 }}
                     />
 
-                  <ControlLabel>Pronouns</ControlLabel>
+                    <ControlLabel>Pronouns</ControlLabel>
                     <FormControl
                       name="pronouns"
                       accepter={RadioGroup}
-                      onChange={(val) => { this.setState({ pronouns: val })}}
+                      onChange={(val) => {
+                        this.setState({ pronouns: val });
+                      }}
                     >
                       <Radio value="He/Him">He/Him</Radio>
                       <Radio value="She/Her">She/Her</Radio>
                       <Radio value="They/Them">They/Them</Radio>
                       <Radio value="Other">Other</Radio>
                     </FormControl>
+                  </FormGroup>
+                </Form>
 
-                </FormGroup>
-              </Form>
-
-              <FlexboxGrid.Item>
-                <Row>
-                  <div className="buttons"></div>
-                  <Button onClick={this.updateProfile} appearance="primary">
-                    Update
-                  </Button>
-                </Row>
-              </FlexboxGrid.Item>
-            </Col>
-          </FlexboxGrid.Item>
-        </FlexboxGrid>
+                <FlexboxGrid.Item>
+                  <Row>
+                    <div className="buttons">
+                      <Button onClick={this.updateProfile} appearance="primary">
+                        Update
+                      </Button>{" "}
+                      <Button
+                        onClick={this.updateProfile}
+                        appearance="primary"
+                        color="green"
+                      >
+                        Cancel
+                      </Button>
+                    </div>
+                  </Row>
+                </FlexboxGrid.Item>
+              </Col>
+            </FlexboxGrid.Item>
+          </FlexboxGrid>
         </ProfileContainer>
       </ProfileEditContainer>
     );
