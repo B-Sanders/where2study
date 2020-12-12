de0d61055ef3d0a6f0359720dfe732eebe2c0fe1
Carlos <ccarvaja@ucsd.edu>
2020-12-12 14:19:21 -0800

    added alt name to logo

--- front-end/src/routes/Login.js
+++ front-end/src/routes/Login.js
@@ -121,7 +121,7 @@ class Login extends React.Component {
         >
           <FlexboxGrid.Item>
             <Col>
-              <img src={logo} height={250} width={300} />
+              <img src={logo} alt="Logo" height={250} width={300} />
               <Form
                 onChange={this.handleChange}
                 formValue={this.state.formValue}
@@ -145,7 +145,10 @@ class Login extends React.Component {
                   />
                   <HelpBlock tooltip>Required</HelpBlock>
                   <br />
-                  <Link onClick={this.handleForgottenPassword}>
+                  <Link
+                    alignItems="right"
+                    onClick={this.handleForgottenPassword}
+                  >
                     Forgot password?
                   </Link>
                 </FormGroup>
